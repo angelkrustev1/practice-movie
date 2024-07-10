3e5412ea05a46e0860453fba2ec364c172ee1f61
angelkrustev1 <angelkrustev07@gmail.com>
2024-07-10 14:39:09 +0300

    async function added

--- functunality.js
+++ functunality.js
@@ -1,8 +1,13 @@
 window.addEventListener("load", solve);
 
 function solve() {
+    let moviesURL = 'http://localhost:3000/movies'
+    let isLoaded = false;
+
     let hideButtonElement = document.getElementById('hide-button');
     let moviesUlElement = document.getElementById('movie-ul');
+    let loadButtonElement = document.getElementById('load-button')
+    let deleteButtonElement = document.getElementById('delete-button')
 
     hideButtonElement.addEventListener('click', () => {
         if (!moviesUlElement.classList.contains('hide')) {
@@ -13,5 +18,30 @@ function solve() {
             moviesUlElement.classList.remove('hide')
             hideButtonElement.textContent = 'Hide'
         }
+    })
+
+    loadButtonElement.addEventListener('click', async () => {
+        let moviesResponse = await fetch(moviesURL)
+        let moviesDate = await moviesResponse.json()
+
+        if (isLoaded) {
+            return
+        }
+
+        for (let movie of Object.values(moviesDate)) {
+            let pictureImgElement = document.createElement('img');
+            pictureImgElement.src = movie.image
+           
+            let pictureLiElement = document.createElement('li')
+
+            pictureLiElement.appendChild(pictureImgElement)
+            moviesUlElement.appendChild(pictureLiElement)
+        }
+        isLoaded = true;
+    })
+
+    deleteButtonElement.addEventListener('click', () => {
+        moviesUlElement.innerHTML = ''
+        isLoaded = false;
     })
   }
